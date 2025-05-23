ecf500b75c845befa445c99cbd34e9fa641c7959
eancheta <cireancheta2003@gmail.com>
2025-05-23 23:30:14 +0800

    1st funtion

---
+++ PrefinalMobSys1/Data/TodoTask.cs
@@ -0,0 +1,7 @@
+public class TodoTask
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime DueDate { get; set; }
+}
