eb212a2b6bd60bf66cf36eb2a9084bb9c770f9c7
eancheta <cireancheta2003@gmail.com>
2025-05-26 22:13:38 +0800

    completed nut no UI disign yet

---
+++ PrefinalMobSys1/Models/ThemeService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrefinalMobSys1.Model
+{
+    public class ThemeService
+    {
+        private readonly IJSRuntime _js;
+
+        public ThemeService(IJSRuntime js)
+        {
+            _js = js;
+        }
+
+        public async Task SetThemeAsync(string theme)
+        {
+            await _js.InvokeVoidAsync("setTheme", theme);
+        }
+    }
+}
